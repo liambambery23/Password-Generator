e6fbec5d3612ee2ea51a660e162eb69f2e1bacd7
Liam B <71141670+liambambery23@users.noreply.github.com>
2020-10-15 08:31:00 -0400

    added prompts and character generating functions

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
+
+let length = prompt("How long do you want your password to be?");
+let uppercase = confirm("Do you want uppercase letters?");
+let lowercase = confirm("Do you want lowercase letters?");
+let numbers = confirm("Do you want numbers?");
+let symbols = confirm("Do you want special characters?");
+
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
@@ -12,3 +19,21 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// Functions to generate characters 
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random()* 26) + 97);
+}
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random()* 26) + 65);
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+  let symbols = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  return symbols[Math.floor(Math.random() * symbols.length)];
