2c9e7430d1186e92e088349daa2359ab11ba69e7
Liam B <71141670+liambambery23@users.noreply.github.com>
2020-10-16 10:50:00 -0400

    fixed functionality to randomly generate password with criteria

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
 
-
-
-//Convert length to number
-
-
-//backend
 //stuff to hold all option for user
 var masterkeyArr= [];
 var lowerArr=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperArr=["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var specialArr=['!','@','#','$','%','^','&','*','(',')','-','_','=','+','/','?','<','>','[',']','{','}','`','~','\\','\"','\'','|',':',';',',','.'];
-var numArr=['0','1','2','3','4','5','6','7','8','9'];
+var specialArr=["!","@","#","$","%","^","&","*","(",")","-","_","=","+","/","?","<",">","[","]","{","}","`","~","\\","\"","\'","|",":",";",",","."];
+var numArr=["0","1","2","3","4","5","6","7","8","9"];
 var userlength;
 
 let generateBtn = document.querySelector("#generate");
@@ -29,22 +23,24 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
-
-
-
-
-  //generates the prompts
   //generates you pw
 function generatePassword() {
+
+  function empty(){
+    masterkeyArr = [];
+  }
+ empty();
   // click the button to generate a password
 
 // series of prompts for password criteria:
 //length
     // 8 characters and no more than 128 characters LATER returns a string => parseFloat
 var userlength=prompt("How long do you want your password to be?");
+  
+
 // lowercase, //return true or false
 var isLower = confirm("Do you want to include lowercase letters?");
+
 //uppercase, 
 var isUpper = confirm("DO you want to include uppercase letters?");
 //numeric, 
@@ -54,17 +50,12 @@ var isSpecial = confirm("Do you want to include special characters?");
 //holds the randomize secret pw
 var finalpswd = '';
 
-
+// converts string to number
 function calclength() {
-  parseFloat(userlength);
-}
+  return parseFloat(userlength);
+ };
 
 var length = calclength()
-
-//how do I build this masterarry specific to each user?
-//confirm add it to the array (masterarry)
-//they say yes to uppers .. what do we do
-
 
 if(isUpper) {
 //add uppers to the masterarray
@@ -92,15 +83,16 @@ if (isSpecial) {
 };
 
 
-for (var x=0;x<length;x++) {
+for (var i=0; i<length; i++) {
   finalpassword(0,masterkeyArr);
   console.log(finalpswd);
   }
 
-  function finalpassword (min ,max) {
+  function finalpassword(min,max) {
     return finalpswd += max[Math.floor(Math.random() * max.length-1)];
   };
   
+  return finalpswd;
 
 }
 
